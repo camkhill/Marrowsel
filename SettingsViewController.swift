9c3ff36e3073b4656043de367e701808d10d5867
Hill <zdr911@9801a7a47c23.cof.ds.capitalone.com>
2016-08-26 15:44:40 -0400

    MVP completed

---
+++ SettingsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SettingsViewController.swift
+//  Marrowsel
+//
+//  Created by Hill, Cameron on 8/26/16.
+//  Copyright © 2016 Hill, Cameron. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var settingsImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = settingsImage.frame.size
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTapX(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
